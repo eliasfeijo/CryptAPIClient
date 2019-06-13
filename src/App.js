d7687ebc77e8a0c9d0f4df6083ad631126f77e7e
Elias Feijó <eliasfeijo.dev@gmail.com>
2019-06-13 06:37:52 -0300

    Rendering ListCryptoCoin component in App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import { registerRootComponent } from 'expo';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import ListCrypoCoin from './components/ListCryptoCoin/ListCryptoCoin'
 
 class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      data: [
+        {id: 1, title: 'Bitcoin', price: 32000.321},
+        {id: 2, title: 'Ethereum', price: 1000.123},
+        {id: 3, title: 'AnotherCoin', price: 4.321},
+        {id: 4, title: 'AnotherCoin', price: 4.321},
+        {id: 5, title: 'AnotherCoin', price: 4.321},
+        {id: 6, title: 'AnotherCoin', price: 4.321},
+        {id: 7, title: 'AnotherCoin', price: 4.321},
+        {id: 8, title: 'AnotherCoin', price: 4.321},
+        {id: 9, title: 'AnotherCoin', price: 4.321},
+        {id: 10, title: 'AnotherCoin', price: 4.321},
+      ]
+    }
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello</Text>
-      </View>
+      <ListCrypoCoin data={this.state.data} />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 registerRootComponent(App);
